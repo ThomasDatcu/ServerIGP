06a1b242313b8e98c55cb2fdaa04f208b88225ae
ThomasDatcu <thomasdatcu@hotmail.fr>
2016-05-02 09:40:38 +0200

    Implémentation Automate

--- src/Server/SocketCommunicationSMTP.java
+++ src/Server/SocketCommunicationSMTP.java
@@ -2,6 +2,7 @@ package Server;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.ArrayList;
 
 /**
  * Created by thomas on 11/04/16.
@@ -9,6 +10,10 @@ import java.net.Socket;
 public class SocketCommunicationSMTP extends SocketCommunication {
     public SocketCommunicationSMTP(Socket s) {
         super(s);
+    }
+
+    public void sendGreetings(String s){
+
     }
 
     @Override
@@ -19,6 +24,8 @@ public class SocketCommunicationSMTP extends SocketCommunication {
         boolean exit = false;
         String textFromClient;
         String[] splitTextFromClient;
+        String clientUserName;
+        ArrayList<User> userArrayList = new ArrayList<>();
         while(!exit){
 
             textFromClient = this.receiveData();
@@ -28,29 +35,63 @@ public class SocketCommunicationSMTP extends SocketCommunication {
             switch (this.state){
 
                 case 0 :
-                    switch(splitTextFromClient[1]){
+                    switch(splitTextFromClient[0]){
                         case "EHLO":
+                            this.writeBytes("250 Server SMTP greets" + splitTextFromClient[1] + ". Our server doesn't have any ESMTP functionality implemented yet");
+                            this.flush();
+                            this.state = 1;
                             break;
                         case "HELO":
+                            this.writeBytes("250 Server SMTP greets" + splitTextFromClient[1]);
+                            this.flush();
+                            this.state = 1;
                             break;
                         default:
                             break;
                     }
                     break;
-
                 case 1 :
-                    switch(splitTextFromClient[1]){
+                    if(splitTextFromClient[0].equals("MAIL") && splitTextFromClient[1].equals("FROM")){
+                        clientUserName = splitTextFromClient[2];
+                        if(this.allUsers.checkUser(clientUserName)!=null){
+                            this.writeBytes("250 OK");
+                            this.flush();
+                            this.state = 2;
+                        }else{
+                            this.writeBytes("501 unknow username");
+                            this.flush();
+                            this.state = 2;
+                        }
+                    }
+
+                case 2 :
+                    switch(splitTextFromClient[0]){
                         case "RCPT":
+                            if(splitTextFromClient[1].equals("TO")){
+                                String receiverUserName = splitTextFromClient[2];
+                                if(allUsers.checkUser(receiverUserName)!=null){
+                                    userArrayList.add(allUsers.getUser(receiverUserName));
+                                    this.writeBytes("250 OK receiver added");
+                                    this.flush();
+                                }else{
+                                    this.writeBytes("550 no such user here");
+                                    this.flush();
+                                }
+                            }
                             break;
                         case "DATA":
+                            this.writeBytes("354 Waiting for e-mail");
+                            this.flush();
+                            this.state = 3;
                             break;
                         default:
                             break;
                     }
                     break;
-                case 2 :
-                    break;
                 case 3 :
+
+                    break;
+                case 4 :
                     exit = true;
                     this.close();
                     break;
